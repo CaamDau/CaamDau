d80003c108574a4d95547244db3935ba3fcad401
LCD <565726319@qq.com>
2019-07-11 09:33:33 +0800

    🔧 LCD - Page 滑块问题

--- CaamDau/Extension/UIImage+CD.swift
+++ CaamDau/Extension/UIImage+CD.swift
@@ -16,7 +16,17 @@ public extension UIImage{
         return UIImage(named: name, in: Bundle.cd_bundle(forClass, from), compatibleWith: nil) ?? UIImage()
     }
     
-    
+    /// - 由颜色 生成图片
+    static func cd_color(_ color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
 }
 
 
