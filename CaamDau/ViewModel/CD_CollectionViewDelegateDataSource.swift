e860c317773141cb256b23446f636d53ed46f6e1
LCD <565726319@qq.com>
2019-07-20 17:37:47 +0800

    🔧 LCD - DelegateDataSource Change

--- CaamDau/ViewModel/CD_CollectionViewDelegateDataSource.swift
+++ CaamDau/ViewModel/CD_CollectionViewDelegateDataSource.swift
@@ -8,6 +8,8 @@
 
 
 import Foundation
+import UIKit
+import SnapKit
 
 //MARK:--- 针对新的表单协议 CD_CellProtocol ----------
 public class CD_CollectionViewDelegateDataSource: NSObject {
@@ -106,5 +108,123 @@ extension CD_CollectionViewDelegateDataSource: UICollectionViewDelegate, UIColle
             row.bind(v)
             return v
         }
+    }
+}
+
+
+
+
+
+
+//MARK:--- 提供一个基础的 CollectionViewController 简单的页面不需要编写 ViewController----------
+public struct R_CDCollectionViewController {
+    public static func push(_ vm:CD_ViewModelCollectionViewProtocol) {
+        let vc = CD_CollectionViewController()
+        vc.vm = vm
+        CD.push(vc)
+    }
+}
+
+class CD_CollectionViewController: UIViewController {
+    var vm:CD_ViewModelCollectionViewProtocol?
+    var delegateData: CD_CollectionViewDelegateDataSource?
+    lazy var collectionView: UICollectionView = {
+        return UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init()).cd
+            .register([.tCell(CD_CollectionViewCellNone.self, nil, nil),
+                       .tView(CD_CollectionReusableViewNone.self, nil, .tHeader, nil),
+                       .tView(CD_CollectionReusableViewNone.self, nil, .tFooter, nil)])
+            .register(vm?._collectionRegisters ?? [])
+            .background(UIColor.lightGray)
+            .build
+    }()
+    lazy var topBar: CD_TopBar = {
+        return CD_TopBar()
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeUI()
+        makeLayout()
+        makeReloadData()
+        topBar.delegate = self
+        vm?._collectionViewCustom?(collectionView)
+    }
+    
+    func makeUI(){
+        self.cd.navigationBar(hidden: true)
+        self.view.cd
+            .add(collectionView)
+            .add(topBar)
+        
+        delegateData = CD_CollectionViewDelegateDataSource(vm)
+        collectionView.cd.delegate(delegateData).dataSource(delegateData)
+        
+    }
+    
+    func makeLayout(){
+        topBar.snp.makeConstraints { (make) in
+            make.left.right.top.equalToSuperview()
+        }
+        
+        collectionView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.top.equalTo(topBar.snp.bottom)
+        }
+    }
+    
+    func makeReloadData() {
+        vm?._reloadData = {[weak self] in
+            self?.collectionView.reloadData()
+            self?.collectionView.cd.mjRefreshTypes(self!.vm?._mjRefreshType ?? [.tEnd])
+        }
+        
+        vm?._reloadDataIndexPath = { [weak self] (indexPath, animation) in
+            self?.collectionView.reloadItems(at: indexPath)
+            self?.collectionView.cd.mjRefreshTypes(self!.vm?._mjRefreshType ?? [.tEnd])
+        }
+        
+        guard let refresh = vm?._mjRefresh else {
+            return
+        }
+        
+        if refresh.header {
+            if refresh.headerGif {
+                self.collectionView.cd.headerMJGifWithModel({[weak self] in
+                    self?.vm?.requestData(true)
+                    }, model: vm!._mjRefreshModel)
+            }else{
+                self.collectionView.cd.headerMJWithModel({[weak self] in
+                    self?.vm?.requestData(true)
+                    }, model: vm!._mjRefreshModel)
+            }
+        }
+        if refresh.footer {
+            if refresh.footerGif {
+                self.collectionView.cd.footerMJGifAutoWithModel({[weak self] in
+                    self?.vm?.requestData(false)
+                    }, model: vm!._mjRefreshModel)
+            }else{
+                self.collectionView.cd.footerMJAutoWithModel({[weak self] in
+                    self?.vm?.requestData(false)
+                    }, model: vm!._mjRefreshModel)
+            }
+        }
+        
+        self.collectionView.cd.mjRefreshTypes(vm!._mjRefreshType)
+    }
+    
+}
+
+extension CD_CollectionViewController: CD_TopBarProtocol {
+    func topBarCustom() {
+        vm?._topBarCustom?(self.topBar)
+    }
+    
+    func didSelect(withTopBar item: CD_TopNavigationBar.Item) {
+        //super_topBarClick(item)
+        vm?._topBarDidSelect?(self.topBar, item)
+    }
+    
+    func update(withTopBar item: CD_TopNavigationBar.Item) -> [CD_TopNavigationBarItem.Item.Style]? {
+        return vm?._topBarUpdate?(self.topBar, item)
     }
 }
--- CaamDau/ViewModel/CD_TableViewDelegateDataSource.swift
+++ CaamDau/ViewModel/CD_TableViewDelegateDataSource.swift
@@ -7,8 +7,9 @@
 
 
 
+import Foundation
 import UIKit
-
+import SnapKit
 
 //MARK:--- 针对新的表单协议 CD_CellProtocol ----------
 public class CD_TableViewDelegateDataSource: NSObject {
@@ -95,3 +96,112 @@ extension CD_TableViewDelegateDataSource: UITableViewDelegate, UITableViewDataSo
 }
 
 
+//MARK:--- 提供一个基础的 TableViewController 简单的页面不需要编写 ViewController----------
+public struct R_CDTableViewController {
+    public static func push(_ vm:CD_ViewModelTableViewProtocol) {
+        let vc = CD_TableViewController()
+        vc.vm = vm
+        CD.push(vc)
+    }
+}
+
+
+class CD_TableViewController: UIViewController {
+    var vm:CD_ViewModelTableViewProtocol?
+    var delegateData:CD_TableViewDelegateDataSource?
+    lazy var tableView: UITableView = {
+        return UITableView(frame: CGRect.zero, style: UITableView.Style.grouped).cd
+            .build
+    }()
+    lazy var topBar: CD_TopBar = {
+        return CD_TopBar()
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeUI()
+        makeLayout()
+        makeReloadData()
+        topBar.delegate = self
+        vm?._tableViewCustom?(tableView)
+    }
+    
+    func makeUI(){
+        self.cd.navigationBar(hidden: true)
+        self.view.cd
+            .add(tableView)
+            .add(topBar)
+        
+        delegateData = CD_TableViewDelegateDataSource(vm)
+        tableView.cd.delegate(delegateData).dataSource(delegateData)
+        
+    }
+    
+    func makeLayout(){
+        topBar.snp.makeConstraints { (make) in
+            make.left.right.top.equalToSuperview()
+        }
+        
+        tableView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.top.equalTo(topBar.snp.bottom)
+        }
+    }
+    
+    func makeReloadData() {
+        vm?._reloadData = {[weak self] in
+            self?.tableView.reloadData()
+            self?.tableView.cd.mjRefreshTypes(self!.vm?._mjRefreshType ?? [.tEnd])
+        }
+        
+        vm?._reloadDataIndexPath = { [weak self] (indexPath, animation) in
+            self?.tableView.reloadRows(at: indexPath, with: animation)
+            self?.tableView.cd.mjRefreshTypes(self!.vm?._mjRefreshType ?? [.tEnd])
+        }
+        
+        guard let refresh = vm?._mjRefresh else {
+            return
+        }
+        
+        if refresh.header {
+            if refresh.headerGif {
+                self.tableView.cd.headerMJGifWithModel({[weak self] in
+                    self?.vm?.requestData(true)
+                    }, model: vm!._mjRefreshModel)
+            }else{
+                self.tableView.cd.headerMJWithModel({[weak self] in
+                    self?.vm?.requestData(true)
+                    }, model: vm!._mjRefreshModel)
+            }
+        }
+        if refresh.footer {
+            if refresh.footerGif {
+                self.tableView.cd.footerMJGifAutoWithModel({[weak self] in
+                    self?.vm?.requestData(false)
+                    }, model: vm!._mjRefreshModel)
+            }else{
+                self.tableView.cd.footerMJAutoWithModel({[weak self] in
+                    self?.vm?.requestData(false)
+                    }, model: vm!._mjRefreshModel)
+            }
+        }
+        
+        self.tableView.cd.mjRefreshTypes(vm!._mjRefreshType)
+    }
+    
+}
+
+extension CD_TableViewController: CD_TopBarProtocol {
+    func topBarCustom() {
+        vm?._topBarCustom?(self.topBar)
+    }
+    
+    func didSelect(withTopBar item: CD_TopNavigationBar.Item) {
+        //super_topBarClick(item)
+        vm?._topBarDidSelect?(self.topBar, item)
+    }
+    
+    func update(withTopBar item: CD_TopNavigationBar.Item) -> [CD_TopNavigationBarItem.Item.Style]? {
+        return vm?._topBarUpdate?(self.topBar, item)
+    }
+}
