00935e668421692b7d95bb587d4457a45cbee997
LCD <565726319@qq.com>
2019-07-20 13:20:29 +0800

    🔧 LCD - Keyboard  BeforeHandle

--- CaamDau/InputBox/CD_Keyboard.swift
+++ CaamDau/InputBox/CD_Keyboard.swift
@@ -12,25 +12,31 @@ import UIKit
 public class CD_Keyboard {
     weak var view:UIView?
     var observers:[NSObjectProtocol] = []
-    open var willShowHandle:((CGFloat) -> Void)?
-    open var willHideHandle:(() -> Void)?
-    public init(view:UIView) {
+    open var willShowBeforeHandle:(() -> Void)?
+    open var willHideBeforeHandle:(() -> Void)?
+    open var willShowCallback:((CGFloat) -> Void)?
+    open var willHideCallback:(() -> Void)?
+    public init(view:UIView, willShowBeforeHandle show:(() -> Void)? = nil, willHideBeforeHandle hide:(() -> Void)? = nil) {
         self.view = view
-        self.willShowHandle = { [weak self] h in
+        self.willShowCallback = { [weak self] h in
             var bottom:CGFloat = 0.0
             if #available(iOS 11.0, *) {
                 bottom = CD.window?.safeAreaInsets.bottom ?? 0
             }
             self?.view?.transform = CGAffineTransform(translationX: 0,y: -h+bottom)
         }
-        self.willHideHandle = { [weak self] in
+        self.willHideCallback = { [weak self] in
             self?.view?.transform = .identity
         }
+        self.willShowBeforeHandle = show
+        self.willHideBeforeHandle = hide
         makeNotification()
     }
-    public init(willShow:((CGFloat) -> Void)?, willHide:(() -> Void)?) {
-        self.willShowHandle = willShow
-        self.willHideHandle = willHide
+    public init(callback willShow:((CGFloat) -> Void)?, willHide:(() -> Void)?, willShowBeforeHandle show:(() -> Void)? = nil, willHideBeforeHandle hide:(() -> Void)? = nil) {
+        self.willShowCallback = willShow
+        self.willHideCallback = willHide
+        self.willShowBeforeHandle = show
+        self.willHideBeforeHandle = hide
         makeNotification()
     }
     public init() {
@@ -40,8 +46,7 @@ public class CD_Keyboard {
 
 
 extension CD_Keyboard {
-    
-    func makeNotification() {
+    private func makeNotification() {
         do{
             let ob = NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { [weak self](n) in
                 self?.keyBoardWillShow(n)
@@ -57,6 +62,7 @@ extension CD_Keyboard {
     }
     
     private func keyBoardWillShow(_ note:Notification) {
+        willShowBeforeHandle?()
         guard let userInfo  = note.userInfo else { return }
         guard let keyBoardBounds = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
         let duration:TimeInterval = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval ?? 0.25
@@ -64,18 +70,19 @@ extension CD_Keyboard {
         let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt
         let options = (curve != nil) ? UIView.AnimationOptions(rawValue: curve!) : [.layoutSubviews, .allowUserInteraction , .beginFromCurrentState]
         UIView.animate(withDuration: duration, delay: 0, options: options, animations: {[weak self] in
-            self?.willShowHandle?(hh)
+            self?.willShowCallback?(hh)
         }, completion: nil)
     }
     
     private func keyBoardWillHide(_ note:Notification)
     {
+        willHideBeforeHandle?()
         guard let userInfo  = note.userInfo else { return }
         let duration:TimeInterval = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval ?? 0.25
         let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt
         let options = (curve != nil) ? UIView.AnimationOptions(rawValue: curve!) : [.layoutSubviews, .allowUserInteraction , .beginFromCurrentState]
         UIView.animate(withDuration: duration, delay: 0, options: options, animations: {[weak self] in
-            self?.willHideHandle?()
+            self?.willHideCallback?()
         }, completion: nil)
     }
 }
